f3b8adaf79c8c3cba89feee3df96015a3249c523
Rajibul-16103404 <16103404@iubat.edu>
2026-07-18 02:33:02 +0600

    Add examples for conditional statements and sum function

--- Conditions/if_else/if_else.go
+++ Conditions/if_else/if_else.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	//Single condition
 	a := 2
 
 	if a > 20 {
@@ -11,5 +13,23 @@ func main() {
 		fmt.Println("a is less than 20")
 	} else {
 		fmt.Println("a is equal to 20")
+	}
+
+	//Double condition
+	age := 25
+	sex := "emale"
+
+	if age >= 18 && sex == "male" {
+		fmt.Println("You are a male and you are eligible to marry")
+	} else if age >= 18 && sex != "female" {
+		fmt.Println("You are a ? and you are eligible to marry")
+	} else {
+		fmt.Println("You are not eligible to marry")
+	}
+
+	//Boolean condition
+	isMarried := false
+	if !isMarried {
+		fmt.Println("You are not married")
 	}
 }
