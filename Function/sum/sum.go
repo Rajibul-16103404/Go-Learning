67b0a93b58d690b4460aa306bc57725c80317c53
Rajibul-16103404 <16103404@iubat.edu>
2026-07-18 02:34:44 +0600

    Refactor sum calculation to use a dedicated function

--- Function/sum/sum.go
+++ Function/sum/sum.go
@@ -2,10 +2,14 @@ package main
 
 import "fmt"
 
+func sum(a int, b int) int {
+	return a + b
+}
+
 func main() {
 	a := 10
 	b := 20
 
-	sum := a + b
-	fmt.Println("Sum of a and b is:", sum)
+	sum := sum(a, b)
+	fmt.Println(sum)
 }
