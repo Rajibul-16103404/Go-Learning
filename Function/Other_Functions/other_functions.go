1e9ac8c7eeabdbb4d8818f0186e16265da79a680
Rajibul-16103404 <16103404@iubat.edu>
2026-07-18 14:15:00 +0600

    Add initial implementation of functions for printing messages

---
+++ Function/Other_Functions/other_functions.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func printMessage() {
+	fmt.Println("Hello, World!")
+}
+
+func inputMessage(message string) {
+	fmt.Println("Welcome to Go Programming: ", message)
+}
+
+func main() {
+	printMessage()
+	inputMessage("Rajibul Islam")
+}
