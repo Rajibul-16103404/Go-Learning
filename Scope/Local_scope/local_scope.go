93060e06ab0d7d60b315fee0a3f2134383895201
Rajibul-16103404 <16103404@iubat.edu>
2026-07-18 23:55:37 +0600

    Add initial implementation of local scope example with conditional output

---
+++ Scope/Local_scope/local_scope.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var (
+	a = 20
+	b = 30
+)
+
+func main() {
+	x := 18
+
+	if x >= 18 {
+		p := 10
+		fmt.Println("I am Mature boy")
+		fmt.Println("I have ", p, "girlfriends!")
+	}
+}
