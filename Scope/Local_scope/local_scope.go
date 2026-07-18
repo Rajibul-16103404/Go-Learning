9afc62a7956dfe58352a5a4fd5b4585ae4e0ebfd
Rajibul-16103404 <16103404@iubat.edu>
2026-07-18 23:59:30 +0600

    Add comments to clarify variable scopes in local_scope.go

--- Scope/Local_scope/local_scope.go
+++ Scope/Local_scope/local_scope.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 var (
-	a = 20
-	b = 30
+	a = 20 //Global scope variable in main package
+	b = 30 //Global scope variable in main package
 )
 
 func main() {
-	x := 18
+	x := 18 //Global scope variable in main function
 
 	if x >= 18 {
-		p := 10
+		p := 10 // local scope variable in main function
 		fmt.Println("I am Mature boy")
 		fmt.Println("I have ", p, "girlfriends!")
 	}
