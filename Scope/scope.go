0061757ab46f7ee357d9779ac148452c61645259
Rajibul-16103404 <16103404@iubat.edu>
2026-07-18 23:30:30 +0600

    Add initial implementation of main application with add function and example usage

---
+++ Scope/scope.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+var (
+	a = 20
+	b = 30
+)
+
+func add(x int, y int) {
+	z := x + y
+	fmt.Println("Sum:", z)
+}
+
+func main() {
+	p := 30
+	q := 40
+
+	add(p, q)
+
+	add(a, b)
+
+	add(a, p)
+
+	add(b, z)
+
+}
