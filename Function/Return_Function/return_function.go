135ecb5dbba0385341400fbeddaa5ef630e7e9b5
Rajibul-16103404 <16103404@iubat.edu>
2026-07-18 14:08:31 +0600

    Refactor getNumbers function and update main execution with new variable values

--- Function/Return_Function/return_function.go
+++ Function/Return_Function/return_function.go
@@ -7,10 +7,20 @@ func add(num1 int, num2 int) int {
 	return sum
 }
 
+func getNumbers(num1 int, num2 int) (int, int) {
+	sum := num1 + num2
+	mul := num1 * num2
+	return sum, mul
+}
+
 func main() {
-	a := 75
-	b := 35
+	a := 70
+	b := 30
 
 	sum := add(a, b)
 	fmt.Println("Sum:", sum)
+
+	p, q := getNumbers(a, b)
+
+	fmt.Println(p, q)
 }
