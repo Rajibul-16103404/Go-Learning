6dd5ae497207ea24e883137da83813c1286ccef9
Rajibul-16103404 <16103404@iubat.edu>
2026-07-18 13:59:50 +0600

    Add add function for sum calculation and example usage

---
+++ Function/Normal_Function/normal_function.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func add(num1 int, num2 int) {
+	sum := num1 + num2
+	fmt.Println("Sum:", sum)
+}
+
+func main() {
+	a := 100
+	b := 200
+	add(a, b)
+	add(50, 70)
+}
